6745b818a3b3fffa68b94b6d646cf21bbd87dd9e
Sumifel <sumifel.loreth@gmail.com>
2025-07-15 16:26:45 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import csv
+
+app = Flask(__name__)
+
+@app.route('/api/productos', methods=['GET'])
+def obtener_productos():
+    productos = {}
+    with open('productos.csv', newline='', encoding='utf-8') as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            sku = fila.get('sku')
+            if sku:
+                productos[sku] = dict(fila)
+    return jsonify(productos)
+
+if __name__ == '__main__':
+    app.run()
